4bda308dcbc89822d9627d95eba22abcec515d9b
Brooklyn92 <Tubanar173russia>
2022-07-22 16:41:35 +0400

    study lesson5

---
+++ lesson1/src/lesson5_function.js
@@ -0,0 +1,30 @@
+// function sum(a,b) {
+//     return a+b;
+// }
+// console.log(sum(5,5)); // => 10
+//=======================================================
+// let name = "Hi here";
+// function sayHiWithUpperCase(name) {
+//     name = name.toUpperCase();
+//     console.log(name);
+// }
+
+// sayHiWithUpperCase(name); // => HI HERE
+
+//=====Объявление параметра в функции по умолчанию===========
+// function sayHiWithName(name, author = "Anonymous") {
+//     const conMng = author + '--' + name;
+//     console.log(conMng);
+// }
+// sayHiWithName("Jonh"); // => Anonymous--Jonh
+
+//============Стрелочная функция===================
+// let sayHello = () => {
+//     console.log("hi");
+// };
+// sayHello();
+//==================================================================
+// const sum =(a,b) => a+b; // возможность прописывать без фигурных скобок
+// sum(6,4); // -> 10
+//===================================================================
+
